077b1a731cb35f8d5331bfa0e00dac6cb830446c
BioWork <bigjarik@gmail.com>
2021-09-16 18:06:02 +0300

    New Abilities to WIP

---
+++ Project_smuzi/Classes/NpcSector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+#pragma warning disable CS0067
+namespace Project_smuzi.Classes
+{
+    public class NpcSector : INotifyPropertyChanged
+    {
+        public string SectorLabel { get; set; }
+        public List<NpcWorker> SectorWorker { get; set; }
+        public ObservableCollection<Product> SectorProducts { get; set; }
+        public NpcSector()
+        {
+            SectorWorker = new List<NpcWorker>();
+            SectorProducts = new ObservableCollection<Product>();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ Project_smuzi/Classes/NpcWorker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+#pragma warning disable CS0067
+namespace Project_smuzi.Classes
+{
+    public class NpcWorker : INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+        public List<NpcSector> Sectors { get; set; }
+        public NpcWorker()
+        {
+            Sectors = new List<NpcSector>();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
